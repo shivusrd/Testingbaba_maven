70aa06355f0147f4c9370eec73d0e7238716cad1
ps6007 <poosingh6007@gmail.com>
2022-03-19 19:07:15 +0530

    assertion add in make my trip

--- src/main/pages/pages/Makemytrip_launch_page.java
+++ src/main/pages/pages/Makemytrip_launch_page.java
@@ -105,7 +105,7 @@ public class Makemytrip_launch_page extends Baselibrary
 		Applicationutility.getscroll(scroll);
 		
 		Thread.sleep(2000);
-
+		assertEquals(applink.isDisplayed(),true);
 		applink.sendKeys("");
 
 		Applicationutility.clickme(submit);
