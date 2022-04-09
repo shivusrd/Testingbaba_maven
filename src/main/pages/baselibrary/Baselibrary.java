21cf8b240908c5e617b0030fe21d9854736b44e7
shivusrd <dubeyshivam890@gmail.com>
2022-04-09 19:21:03 +0530

    Hi

--- src/main/pages/baselibrary/Baselibrary.java
+++ src/main/pages/baselibrary/Baselibrary.java
@@ -53,9 +53,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-
-
-
 public class Baselibrary 
 {
 
