54c903031ca08cbcfb152977ba3f8216abe2cb9a
shivusrd <dubeyshivam890@gmail.com>
2022-04-30 11:25:39 +0530

    hi

--- src/main/pages/pages/Makemytrip_hotel_page.java
+++ src/main/pages/pages/Makemytrip_hotel_page.java
@@ -7,79 +7,89 @@ import org.openqa.selenium.support.PageFactory;
 import applicationutility.Applicationutility;
 import baselibrary.Baselibrary;
 
-public class Makemytrip_Search_Flight extends Baselibrary
+public class Makemytrip_hotel_page extends Baselibrary
+
 
 {
-	public Makemytrip_Search_Flight() {
-
+	
+	public  Makemytrip_hotel_page() 
+	{
+		
 		PageFactory.initElements(driver, this);
 	}
-
+	
 	@FindBy(xpath = "//span[@class='langCardClose']")
 	private WebElement closebtn;
-
+	
 	@FindBy(css = ".makeFlex.hrtlCenter.font10.makeRelative.lhUser.userLoggedOut")
 	private WebElement login3;
-
-	@FindBy(xpath = "//a[contains(@href,'www.makemytrip.com/flights')]/span[contains(@class,'chFlights')]")
-	WebElement flightsMenu;
-
+	
+	@FindBy(xpath ="(//*[@class=\"false chNavText darkGreyText\"])[2]")
+	WebElement hotel;
+	
+	
 	@FindBy(xpath = "//input[@placeholder='Enter city/ Hotel/ Area/ Building']")
 	private WebElement chooseplace;
-
-	@FindBy(xpath = "//button[@id='hsw_search_button']")
+	
+	@FindBy(xpath="//button[@id='hsw_search_button']")
 	private WebElement searchbtn;
-
+	
+	@FindBy(xpath = "//div[@aria-label='Sat Mar 26 2022']")
+	private WebElement choosedate;
+	
+	@FindBy(xpath = "//div[@class='DayPicker-Day DayPicker-Day--start DayPicker-Day--selected DayPicker-Day--disabled']")
+	private WebElement checkoutdate;
 	@FindBy(xpath = "//span[normalize-space()='City / Hotel / Area / Building']")
 	private WebElement from;
-
-	@FindBy(xpath = "//span[normalize-space()='DEPARTURE']")
+	
+	@FindBy(xpath = "//input[@id='checkin']")
+	private WebElement checkin;
+	
+	@FindBy(xpath = "//input[@id='checkout']")
+	private WebElement checkout;
+	
+	@FindBy(xpath="//input[@id='checkin']")
 	WebElement departureDrop;
-
-	@FindBy(xpath = "//span[normalize-space()='RETURN']")
+	
+	@FindBy(xpath="//input[@id='checkout']")
 	WebElement returnDrop;
-
+	
 	String departureDate = "//div[@aria-label='%replace%' and @aria-disabled='false']";
 	String returnDate = "//div[@aria-label='%replace%' and @aria-disabled='false']";
-
-	@FindBy(xpath = "//a[contains(@class,'widgetSearchBtn') and text()='Search']")
+	
+	@FindBy(xpath="//button[@id='hsw_search_button']")
 	WebElement searchBtnn;
-
-	public void ClickOnFlight() throws InterruptedException {
-
+	
+	
+	public void ClickOnHotel() throws InterruptedException 
+	{
+		
 		closebtn.click();
 		login3.click();
-		flightsMenu.click();
+		hotel.click();
 
-	}
-
-	public void enterDepartureDate() throws InterruptedException {
-
-		departureDrop.click();
 		
-		Thread.sleep(2000);
-
-		Applicationutility date = Applicationutility.getCurrentAndReturnDates();
-		Thread.sleep(2000);
-		driver.findElement(Applicationutility.customXpath(departureDate, date.departureDate)).click();
-		Thread.sleep(2000);
 	}
-
-	public void enterReturnDate() throws InterruptedException {
-
+	
+	public void enterDepartureDate() throws InterruptedException {
+		departureDrop.click();
+	
+		Applicationutility date = Applicationutility.getCurrentAndReturnDates();
+		driver.findElement(Applicationutility.customXpath(departureDate,date.departureDate)).click();
+	}
+	
+	public void enterReturnDate() {
 		returnDrop.click();
-		Applicationutility.scrollToView(returnDrop);
-		Thread.sleep(2000);
 		Applicationutility date = Applicationutility.getCurrentAndReturnDates();
-		Thread.sleep(2000);
 		driver.findElement(Applicationutility.customXpath(returnDate, date.returnDate)).click();
-		Thread.sleep(2000);
 	}
 
-	public void clicksearch() {
+	
+	
+	public void  clicksearch () {
 		searchBtnn.click();
-		driver.manage().deleteAllCookies();
-
+		
+		
 	}
 
 }
---
+++ src/main/test/database/ConnectMySql.java
@@ -0,0 +1,18 @@
+package database;
+
+import org.testng.annotations.Test;
+
+public class ConnectMySql
+
+{
+	
+	@Test
+	public void TestDB() throws ClassNotFoundException
+	
+	
+	{
+		
+		Class.forName("");
+	}
+
+}
