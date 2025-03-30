7f50c82c224eb33c050a59fdea4c47c454dede7f
shivusrd <dubeyshivam890@gmail.com>
2025-03-30 18:03:28 +0530

    jenkins

--- src/main/test/jenkins/Jenkins_launch_test.java
+++ src/main/test/jenkins/Jenkins_launch_test.java
@@ -11,49 +11,27 @@ import propertyutility.PropertyUtility;
 
 public class Jenkins_launch_test extends Baselibrary
 
-
 {
 
 	Jenkins_launch_page ob;
+
 	@Parameters({ "browser" })
 	@BeforeTest
 
-	public void launchFB(String browser) 
-	
+	public void launchFB(String browser)
+
 	{
 		String url = PropertyUtility.getreadproperty("jenkins");
-		if(browser.equalsIgnoreCase("firefox")) {
-			 
-		     //Initializing the firefox driver (Gecko)
-			 FirefoxLaunch();
-
-		  }else if (browser.equalsIgnoreCase("chrome")) { 
-
-			  //Initialize the chrome driver
-
-			  ChromeLaunch();
-
-		  } 
-		  else if (browser.equalsIgnoreCase("edge")) { 
-
-			  //Initialize the chrome driver
-
-			 EdgeLaunch();
-
-		  } 
 		driver.get(url);
 		ob = new Jenkins_launch_page();
-	    
+
 	}
-	
+
 	@Test
-   public void LaunchTestingBaba_test01() throws Exception
-   {
-		
+	public void LaunchTestingBaba_test01() throws Exception {
+
 		ob.Launch_Jenkins();
-		
-   }
 
-
+	}
 
 }
